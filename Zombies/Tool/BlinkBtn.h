08ad369f0ef9e4b0385cead7dea867dc5d2d7cf2
123456qwer <346607313@qq.com>
2017-07-05 15:56:29 +0800

    新增加了闪现技能

---
+++ Zombies/Tool/BlinkBtn.h
@@ -0,0 +1,13 @@
+//
+//  BlinkBtn.h
+//  Zombies
+//
+//  Created by 吴冬 on 2017/7/5.
+//  Copyright © 2017年 北京金源互动科技有限公司. All rights reserved.
+//
+
+#import "FireBtn.h"
+
+@interface BlinkBtn : FireBtn
+
+@end
---
+++ Zombies/Tool/BlinkBtn.m
@@ -0,0 +1,21 @@
+//
+//  BlinkBtn.m
+//  Zombies
+//
+//  Created by 吴冬 on 2017/7/5.
+//  Copyright © 2017年 北京金源互动科技有限公司. All rights reserved.
+//
+
+#import "BlinkBtn.h"
+
+@implementation BlinkBtn
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
